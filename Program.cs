652fea7801ce0f284accaf0b9532df2e3c001232
Buriiboo <Buraian@live.se>
2023-10-25 10:48:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,11 +25,41 @@ namespace Mainmenu
                 switch(choice)
                 {
                     case "1":
-                        System.Console.WriteLine("");
+                        //character creator -> name, class, 
+                        Console.Clear();
+                        Console.Write("Chose a name: ");
+                        string characterName = Console.ReadLine();
+
+                        int characterHP, physDamage, magicDamage, exp = 0, lvl = 1;
+
+                        Console.Clear();
+                        int characterClassChoice;
+                        do
+                        {
+                            Console.WriteLine("Välj din klass: ");
+                            Console.WriteLine("1) Riddare");
+                            Console.WriteLine("2) Trolleri-are");
+                        } while (!int.TryParse(Console.ReadLine(), out characterClassChoice) || (characterClassChoice != 1 && characterClassChoice != 2));
+
+                        if (characterClassChoice == 1)
+                        {   // Riddare
+                            characterHP = 120;
+                            physDamage = 20;
+                            magicDamage = 2;
+                        }
+                        else if (characterClassChoice == 2)
+                        {   // Trolleri-are
+                            characterHP = 80;
+                            physDamage = 2;
+                            magicDamage = 20;
+                        }
+                        
                         break;
                     case "2":
+                        //Load game. json?
                         break;
                     case "3":
+                        //????
                         break;
                     case "4":
                         return;
