8e1b0cdc5e2a1c2548f79c9710ba24e2fe47a02d
Buriiboo <Buraian@live.se>
2023-10-25 09:50:43 +0200

    start menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+
+namespace Mainmenu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            bool runMain = true;
+            while (runMain)
+            {
+
+                
+                Console.WriteLine("|| ===================== ||");
+                Console.WriteLine("|| CLIFFS OF COCKERMOUTH ||");
+                Console.WriteLine("|| ======================||");
+                Console.WriteLine("|| 1) New game           ||");
+                Console.WriteLine("|| 2) Load game          ||");     
+                Console.WriteLine("|| 3) ?????????          ||");
+                Console.WriteLine("|| 4) Exit               || ");
+                Console.WriteLine("|| ===================== ||");
+                string choice = Console.ReadLine();
+                
+                switch(choice)
+                {
+                    case "1":
+                        System.Console.WriteLine("");
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        return;
+                }
+            }
+
+        }
+    }
+}
